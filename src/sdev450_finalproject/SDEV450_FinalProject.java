9700771d82261b48b48725c18bc87f5cbc8a300d
Dev <DevDeCoste@users.noreply.github.com>
2019-01-18 19:16:50 -0500

    Initial commit

---
+++ src/sdev450_finalproject/SDEV450_FinalProject.java
@@ -0,0 +1,21 @@
+package sdev450_finalproject;
+
+/** 
+ * @Course: SDEV 350 ~ Java Programming I
+ * @Author Name: Dev
+ * @Assignment Name: sdev450_finalproject
+ * @Date: Jan 18, 2019
+ * @Description: 
+ */
+
+//Imports
+import javafx.application.Application;
+import javafx.stage.Stage;
+//Begin Class SDEV450_FinalProject
+public class SDEV450_FinalProject extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+} //End Class SDEV450_FinalProject
