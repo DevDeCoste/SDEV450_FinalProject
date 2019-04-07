421f1805f689135e7c8cc031d602ff12931bee50
Dev <DevDeCoste@users.noreply.github.com>
2019-04-07 16:16:02 -0400

    Updated AlbumController

--- src/main/java/com/sdev450_finalproject/Controller/AlbumController.java
+++ src/main/java/com/sdev450_finalproject/Controller/AlbumController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.opencsv.CSVReader;
 import com.sdev450_finalproject.persistance.Album.AlbumEntity;
 
+
 @RestController
 @RequestMapping("")
 public class AlbumController {
+
+    static String FILE_PATH = "CSVFiles/albumlist.csv";
+
 
 //    @GetMapping("/loadAlbum")
 //    public String LoadAlbum() {
