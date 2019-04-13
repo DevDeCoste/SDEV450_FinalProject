b82155736159fba9bf3e26c017e2b968145cf2cb
Madeline Milagros <MMMerced@gmail.com>
2019-04-13 14:26:31 -0400

    Fixing Issues between ArtistEntity and Repository
    Got a small method to work properly but still having issues with the main find artists method

--- src/main/java/com/sdev450_finalproject/Controller/ArtistController.java
+++ src/main/java/com/sdev450_finalproject/Controller/ArtistController.java
@@ -9,7 +9,9 @@ package com.sdev450_finalproject.Controller;
  */
 //Imports
 import com.opencsv.CSVReader;
+import com.sdev450_finalproject.persistance.Album.AlbumEntity;
 import com.sdev450_finalproject.persistance.Artist.ArtistEntity;
+import com.sdev450_finalproject.persistance.Artist.ArtistRepository;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -20,9 +22,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,114 +36,92 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("")
 public class ArtistController {
 
-    static String FILE_PATH = "./src/main/resources/albumlist.csv";
+    static String FILE_PATH = "./src/main/resources/MasterCSV.csv";
 
-    @GetMapping("/findArtist/{artistName}")
-    public ArrayList<ArtistEntity> findArtist(@PathVariable("artistName") String searchArtist)
-            throws IOException, ParseException {
-
-        searchArtist = "artistName";
-
-        //Opens reader to read CSV file and set table values
-        try (Reader reader = Files.newBufferedReader(Paths.get(FILE_PATH));
-                CSVReader csvReader = new CSVReader(reader);) {
-
-            String[] nextRecord;
-            ArrayList<ArtistEntity> artists = new ArrayList<>();
-
-            while ((nextRecord = csvReader.readNext()) != null) {
-                ArtistEntity tempArtist = new ArtistEntity();
-
-                if (nextRecord[nextRecord.length - 2].contains("artistName")) {
-
-                    tempArtist.setId(nextRecord[0]);
-                    tempArtist.setArtistName(nextRecord[3]);
-                    tempArtist.setAlbumName(nextRecord[2]);
-//                    tempArtist.setTracks(nextRecord);
-                    tempArtist.setGenre(nextRecord[4]);
-
-                    artists.add(tempArtist);
-
-                }
-
-            }
-
-            csvReader.close();
-
-            if (artists.isEmpty()) {
-                Reader reader1 = Files.newBufferedReader(Paths.get(FILE_PATH));
-                CSVReader csvReader1 = new CSVReader(reader1);
-
-                ArtistEntity tempArtist = new ArtistEntity();
-
-                int i = 0;
-
-                int randInt = new Random().nextInt(100);
-
-                while (i <= randInt) {
-                    nextRecord = csvReader1.readNext();
-                    i = i + 2;
-                }
-
-//				
-                System.out.println(Arrays.toString(nextRecord));
-                tempArtist.setId(nextRecord[0]);
-                tempArtist.setArtistName(nextRecord[3]);
-                tempArtist.setAlbumName(nextRecord[2]);
-//                tempArtist.setTracks(nextRecord);
-                tempArtist.setGenre(nextRecord[4]);
-
-                artists.add(tempArtist);
-                csvReader1.close();
-
-            }
-            search(searchArtist);
-            return artists;
-        }
-    }
-
-    public static void search(String searchArtist) {
-        try {
-
-            String strSearch = "Nirvana";
-
-            List<String[]> myEntries;
-            try (CSVReader reader = new CSVReader(new FileReader(FILE_PATH), ',')) {
-                myEntries = reader.readAll();
-                System.out.println(myEntries.size());
-            }
-
-            //Write to existing file
-            //CSVWriter writer = new CSVWriter(new FileWriter("test.txt"), ',',CSVWriter.NO_QUOTE_CHARACTER, "\r\n");
-            //Iterate through my array to find the row the user input is located on
-            int i = 1;
-            for (String[] line : myEntries) {
-
-                String textLine = Arrays.toString(line).replaceAll("\\[|\\]", "");
-                System.out.println(textLine);
-                if (textLine.contains(strSearch)) {
-                    //Need a method so I can find out what row my item is on
-                    System.out.println("Found - Your item is on row: ...:" + i);
-
-                    //Code to extract row into something (String array?) so I can edit it's contents
-                    //I then need to write the edited row back to it's original row
-                } else {
-                    System.out.println("Not found");
-                }
-                i++;
-            }
-
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-    }
+    @Autowired
+    ArtistRepository repository;
     
-    	@RequestMapping("/developer/{id}")
-	public String developer(@PathVariable Long id, Model model) {
-            model.addAttribute(id);
-//		model.addAttribute("developer", repository.findOne(id));
-//		model.addAttribute("skills", skillRepository.findAll());
-		return "developer";
-	}
+    @GetMapping(path = "/artists")
+    public List<ArtistEntity> getEntities() {
+        return repository.findAll();
+    }
+
+    @PostMapping(path = "/artists")
+    public boolean createArtist(@RequestBody ArtistEntity artistEntity) {
+        //findAlbum(@PathVariable(albumLists));
+        repository.save(artistEntity);
+        return true;
+    }
+
+    @GetMapping("/findArtists/findOne")
+    public ArrayList<ArtistEntity> findOne(){
+        return repository.findByArtistEquals("Jay Z");
+    }
+
+    
+//    
+//    @GetMapping("/findArtist/{artistName}")
+//    public ArrayList<ArtistEntity> findArtist(@PathVariable("artistName") 
+//            String searchArtist) throws IOException{
+//
+//        //Opens reader to read CSV file and set table values
+//        try (Reader reader = Files.newBufferedReader(Paths.get(FILE_PATH));
+//                CSVReader csvReader = new CSVReader(reader);) {
+//
+//            String[] nextRecord;
+//            
+//            ArrayList<ArtistEntity> artistLists = new ArrayList<>();
+//
+//            while ((nextRecord = csvReader.readNext()) != null) {
+//                ArtistEntity tempArtist = new ArtistEntity();
+//
+//                if (nextRecord[2].equalsIgnoreCase(searchArtist)) {
+//
+//                    tempArtist.setArtist(nextRecord[2]);
+//                    tempArtist.setAlbumName(nextRecord[3]);
+//                    tempArtist.setGenre(nextRecord[4]);
+//
+//                    artistLists.add(tempArtist);
+//              
+//                    if (repository.findByArtistNameEquals(tempArtist.getArtist()) 
+//                            == null) {
+//                        repository.save(tempArtist);
+//                    }
+//                }
+//
+//            }
+//
+//            csvReader.close();
+//
+//            if (artistLists.isEmpty()) {
+//                Reader reader1 = Files.newBufferedReader(Paths.get(FILE_PATH));
+//                CSVReader csvReader1 = new CSVReader(reader1);
+//
+//                ArtistEntity tempArtist = new ArtistEntity();
+//
+//                int i = 0;
+//
+//                int randInt = new Random().nextInt(100);
+//
+//                while (i <= randInt) {
+//                    nextRecord = csvReader1.readNext();
+//                    i = i + 2;
+//                }
+//
+////				
+//                System.out.println(Arrays.toString(nextRecord));
+//                tempArtist.setArtist(nextRecord[2]);
+//                tempArtist.setAlbumName(nextRecord[3]);
+//                tempArtist.setGenre(nextRecord[4]);
+//
+//                artistLists.add(tempArtist);
+//                repository.save(tempArtist);
+//                csvReader1.close();
+//
+//            }
+//            
+//            return artistLists;
+//        }
+//    }
 }//End Subclass ArtistController
 
