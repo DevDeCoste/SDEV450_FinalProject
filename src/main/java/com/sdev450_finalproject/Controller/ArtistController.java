efa09b395b9d4dc6e4f155dfad2a062a556d261d
Madeline Milagros <MMMerced@gmail.com>
2019-04-15 17:07:02 -0400

    Update ArtistController - Fixed If/Else in search

--- src/main/java/com/sdev450_finalproject/Controller/ArtistController.java
+++ src/main/java/com/sdev450_finalproject/Controller/ArtistController.java
@@ -145,15 +145,12 @@ public class ArtistController {
             for (String[] line : myEntries) {
 
                 String textLine = Arrays.toString(line).replaceAll("\\[|\\]", "");
-                System.out.println(textLine);
                 if (textLine.contains(strSearch)) {
                     //Need a method so I can find out what row my item is on
-                    System.out.println("Found - Your item is on row: ...:" + i);
-
+                                    System.out.println(textLine);
                     //Code to extract row into something (String array?) so I can edit it's contents
                     //I then need to write the edited row back to it's original row
                 } else {
-                    System.out.println("Not found");
                 }
                 i++;
             }
