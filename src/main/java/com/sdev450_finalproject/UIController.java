05be2d7c066a669581680559e7f164e24b469884
Dev <DevDeCoste@users.noreply.github.com>
2019-04-22 14:11:41 -0400

    Working JavaFX and Spring and UI

--- src/main/java/com/sdev450_finalproject/UIController.java
+++ src/main/java/com/sdev450_finalproject/UIController.java
@@ -4,8 +4,11 @@ import javafx.application.HostServices;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class UIController {
@@ -26,8 +29,29 @@ public class UIController {
     public HBox buttonBox;
 
     @FXML
+    public TextArea text;
+
+    @FXML
+    public Button searchAlbum;
+
+    @FXML
+    public TextField textField;
+
+    @FXML
     public void initialize() {
-        this.button.setOnAction( actionEvent ->
-                this.label.setText(this.hostServices.getDocumentBase()));
-    }
+        this.button.setOnAction( actionEvent -> {
+            RestTemplate restTemplate = new RestTemplate();
+            String data = restTemplate.getForEntity("http://localhost:8085/albums", String.class ).getBody();
+            this.text.setText(data);
+            }
+        );
+        this.searchAlbum.setOnAction( actionEvent -> {
+                    RestTemplate restTemplate = new RestTemplate();
+                    String input = textField.getText();
+                    String data = restTemplate.postForEntity("http://localhost:8085/findTracksInAlbum/"+ input, null, String.class ).getBody();
+                    this.text.setText(data);
+                }
+        );
+}
+
 }
