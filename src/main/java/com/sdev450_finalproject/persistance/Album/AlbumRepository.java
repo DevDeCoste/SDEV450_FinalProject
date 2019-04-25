b9253639df707ea5b1612ec5357035bb98f8a319
Dev <DevDeCoste@users.noreply.github.com>
2019-04-25 16:57:29 -0400

    Removed unnecessary artist variable in the AlbumEntity and Controller to clean up code output

--- src/main/java/com/sdev450_finalproject/persistance/Album/AlbumRepository.java
+++ src/main/java/com/sdev450_finalproject/persistance/Album/AlbumRepository.java
@@ -1,9 +1,12 @@
 package com.sdev450_finalproject.persistance.Album;
 
+import com.sdev450_finalproject.persistance.ArtistEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
 
 @Transactional
 public interface AlbumRepository extends JpaRepository<AlbumEntity, String> {
@@ -12,7 +15,7 @@ public interface AlbumRepository extends JpaRepository<AlbumEntity, String> {
 
     AlbumEntity findByAlbumNameEquals(String AlbumName);
 
-//    ArrayList<AlbumEntity> findAllByArtist(String ArtistName);
+    ArrayList<ArtistEntity> findAllByArtist(String ArtistName);
     
      
 
